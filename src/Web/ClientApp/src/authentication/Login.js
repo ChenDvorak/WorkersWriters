4a23e11cc4399665e5767de5e943e9dd08c4963d
chenbainan <birenchens@163.com>
2020-11-12 22:44:49 +0800

    init login component

--- src/Web/ClientApp/src/authentication/Login.js
+++ src/Web/ClientApp/src/authentication/Login.js
@@ -1,14 +1,41 @@
-﻿import React, { Component } from 'react';
-import { Box, Button } from '@material-ui/core'
+﻿import React, { useState } from 'react';
+import { Box, Button, Container, TextField } from '@material-ui/core'
 
-export class Login extends Component {
-    render() {
-        return ( <div>
-            login box
-            <Box component='div' color='text.primary' clone>
-                <Button variant='contained'>按钮</Button>
-            </Box> 
+import './Login.css';
+
+export function Login(props) {
+
+    const [step, setStep] = useState(LOGIN_STEP.InputAccount)
+
+    return (
+        <Container maxWidth='xs'>
+            <div className='logo'>
+                Workers Writers
             </div>
-        );
-    }
+            {
+                step === LOGIN_STEP.InputAccount 
+                ? <AccountBox nextStepEvent={() => {setStep(LOGIN_STEP.InputPassword)}} />
+                : <TextField label='Password' type='password' fullWidth margin='normal' />
+            }
+            
+            
+        </Container>
+    );
 }
+
+function AccountBox(props) {
+
+    return (
+        <Box>
+            <TextField label='Account' fullWidth margin='normal' />
+                <Button 
+                    variant='contained' color='primary'
+                    onClick={() => {props.nextStepEvent()}}>按钮</Button>
+        </Box> 
+    );
+}
+
+const LOGIN_STEP = {
+    InputAccount: 0,
+    InputPassword: 1
+};
